5842587562eee9a37f4df91a982021b94c0b2dd4
clement <clementlaukaiming@gmail.com>
2024-03-26 21:08:30 +0800

    userclass update

---
+++ Petto/web/newPackage/User.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newPackage;
+
+/**
+ *
+ * @author Acer
+ */
+public class User {
+    private String userID;
+    private String userName;
+    private String pswd;
+    private String firstName;
+    private String lastName;
+    private boolean isAdmin;
+    public User(String userID,String userName,String pswd,String firstName,String  lastName,String isAdmin){
+        this.userID = userID;
+    }
+    
+}
