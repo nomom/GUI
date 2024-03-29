1d675736ddb3bf3f77141f71cbef61c9e526ca71
clement <clementlaukaiming@gmail.com>
2024-03-29 20:03:19 +0800

    Created database folder, also created some classes

---
+++ Petto/test/javade/Cart.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javade;
+
+/**
+ *
+ * @author Acer
+ */
+public class Cart {
+    private String cartID;
+    private String UserID;
+    private String productID;
+}
